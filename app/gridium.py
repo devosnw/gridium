304a34e589f023cd2d6111e6e86be948a08e83cb
Nic DeVos <nic@devos.me>
2018-02-03 20:44:15 -0800

    Add API request code

--- app/gridium.py
+++ app/gridium.py
@@ -1,6 +1,15 @@
 import flask
+import requests
 
+API_URL = ('https://snapmeter.com/api/v2/531e19848df5cb0b35014e85/meters'
+           '/2166484536790/bill-summary?token=6d547442-417b-41a3-8838-'
+           'b022f9c2974d')
 app = flask.Flask(__name__)
+
+
+def get_bills():
+    return requests.get(API_URL).json()
+
 
 @app.route('/')
 def home():
--- test/test_gridium.py
+++ test/test_gridium.py
@@ -8,5 +8,12 @@ class Test(unittest.TestCase):
     def test_app_is_exposed(self):
         self.assertIsInstance(gridium.app, flask.Flask)
 
+    def test_get_bills(self):
+        self.assertIsInstance(gridium.get_bills(), dict)
+
+    def test_get_bills_data(self):
+        self.assertIsInstance(gridium.get_bills()['data'], list)
+
+
 if __name__ == '__main__':
     unittest.main()
