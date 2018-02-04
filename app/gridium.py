1a536f36f08b6ab928380ac8b59cc016ed9bf2b1
Nic DeVos <nic@devos.me>
2018-02-03 20:56:10 -0800

    Add first layout and views

--- app/gridium.py
+++ app/gridium.py
@@ -1,10 +1,11 @@
-import flask
 import requests
+
+from flask import Flask, render_template
 
 API_URL = ('https://snapmeter.com/api/v2/531e19848df5cb0b35014e85/meters'
            '/2166484536790/bill-summary?token=6d547442-417b-41a3-8838-'
            'b022f9c2974d')
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 
 def get_bills():
@@ -13,7 +14,7 @@ def get_bills():
 
 @app.route('/')
 def home():
-    return "Hello World!"
+    return render_template('home/index.html')
 
 
 if __name__ == '__main__':
